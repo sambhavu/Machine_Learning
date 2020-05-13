f5fd387d453b820c4780a567e4079183d611fef8
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-14 02:45:09 +0530

    Update read csv.py

--- read csv.py
+++ read csv.py
@@ -3,3 +3,6 @@ import pandas as pd
 df=pd.readcsv_csv(data.csv)
 print(df.head) 
 print(df.describe())
+
+price = df['Price'].values #array of stock prices into array from df 
+print(price.shape) # print size of array 
