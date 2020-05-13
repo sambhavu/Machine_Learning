64552f15a002b05d7c645172b280ed7d62077049
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-14 02:29:00 +0530

    Create statistics.py

---
+++ statistics.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+data = [20, 35, 64, 98, 103, 23, 45] 
+
+print("mean: ", np.mean(data))
+print("median: ", np.median(data))
+print("50th percentile: ",np.percentile(data,50))
+print("25th percentile: ",np.percentile(data,25))
+print("75th percentile: ",np.percentile(data,75))
+print("standard deviation: ",np.std(data))
+print("variance: ",np.variance(data)) 
+
