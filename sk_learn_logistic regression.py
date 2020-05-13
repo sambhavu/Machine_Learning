1a97851068c3cbd0ceaddccd985e8060010d84b8
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-14 03:30:42 +0530

    Create sk_learn_logistic regression.py

---
+++ sk_learn_logistic regression.py
@@ -0,0 +1,20 @@
+import pandas as py 
+from sklearn.linear_model import LogisticRegression
+
+df=read_csv(data.csv) 
+
+df['male']= df['sex']=='male' #convert all columns to numerical values 
+X=df[['Pclass','male','age','siblings/spouses','parents/children','fare']].values 
+#convert all interested predictive variables into X array 
+
+y=df['survived'].values
+#put all target in array 
+
+model=LogisticRegression()
+
+model.fit(X,y) 
+
+print(model.coef_, model.intercept_)
+
+
+
