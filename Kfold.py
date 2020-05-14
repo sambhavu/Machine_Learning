39980d97c9176bfdfd482b1be497e07480bf0e61
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-14 12:29:01 +0530

    Update Kfold.py

--- Kfold.py
+++ Kfold.py
@@ -3,3 +3,26 @@ from sklearn.model_selection import Kfold
 from sklearn.linear_model import LogisticRegression 
 
 
+df=pd.read_csv('data.csv')
+df['male']=df['Sex']=='male'
+X=df[['Pclass', 'Male', 'Age', 'Sibling/spouses', 'Parents/Children', 'Fare']].values 
+y=df['Survived'].values 
+
+kf=KFold(n_splits=5, shuffle=True) 
+
+splits=list(kf.split(X))
+
+train_indices, test_indices=splits[0]
+
+X_train=X[train_indices]
+X_test=X[test_indices]
+y_train=y[train_indices]
+y_test=y[test_indices]
+
+model=LogisticRegression() 
+
+model.fit(X_train,y_train) 
+
+print(model.score(X_test,y_test)
+
+
