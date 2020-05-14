2d2f4b620ea7975a502f1da20eda7c54aa27eec4
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-14 09:56:21 +0530

    Create training & testing.py

---
+++ training & testing.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+from sklearn.linear_model import LogisticRegression 
+from sklearn.model_selection import train_test_split 
+
+df = pd.read_csv('data.csv')
+
+df['male']=df['Sex']=='male'
+
+X=df[['Pclass','male','Age','Sibling/spouses', 'parents/children','Fare']].values
+y=df['Survived'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X,y) 
+
+
+
