8b692f9981b1737ef75acb374199cf32724f095d
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-14 10:24:06 +0530

    Update training & testing.py

--- training & testing.py
+++ training & testing.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 from sklearn.linear_model import LogisticRegression 
 from sklearn.model_selection import train_test_split 
+from skleaarn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
 
 df = pd.read_csv('data.csv')
 
@@ -12,5 +14,14 @@ y=df['Survived'].values
 X_train, X_test, y_train, y_test = train_test_split(X,y) 
 
 
+model=LogisticRegression() 
+model=fit(X_train, y_train) 
+y_predict=model.predict(X_test) 
 
-i'm
+print("precision: ", precision_score(y_test, y_predict)) 
+print("accuracy: ", accuracy_score(y_test, y_predict)) 
+print("recall: ", recall_score(y_test, y_predict))
+print("f1: ", f1_score(y_test, y_predict))
+
+
+
